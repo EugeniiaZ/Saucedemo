20d11970f71eeeb5ae0851a4e154d7215213df4b
EugeniiaZ <qaeugeniia@gmail.com>
2025-04-18 22:00:13 +0300

    Login Test

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.*;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
+    CheckoutPage checkoutPage;
+    OverviewPage overviewPage;
+    FinishPage finishPage;
+
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutPage = new CheckoutPage(driver);
+        overviewPage = new OverviewPage(driver);
+        finishPage = new FinishPage(driver);
+
+
+    }
+
+
+
+    @AfterMethod
+    public void close(){
+       driver.quit();
+    }
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void checkLoginWithWrongPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "123456789");
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void checkLoginWithEmptyPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+        Assert.assertEquals(loginPage.getErrorMessage(),
+                "Epic sadface: Password is required");
+
+    }
+    @Test
+    public void checkLoginWithValidCred() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        Assert.assertTrue(productsPage.titleIsDisplayed());
+    }
+}
